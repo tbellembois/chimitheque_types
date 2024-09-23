63a4d05d6a3c0aa48e085b856734e62c943aefe1
Thomas Bellembois <thomas.bellembois@uca.fr>
2024-09-23 16:55:38 +0200

    Add Productclassofcompound.

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,7 @@ pub mod precautionarystatement;
 pub mod producer;
 pub mod producerref;
 pub mod product;
+pub mod productclassofcompound;
 pub mod productsynonyms;
 pub mod producttype;
 pub mod pubchemproduct;
