297000b3bbe5e6bdd25d05be5815debf272357d1
Thomas Bellembois <thomas.bellembois@uca.fr>
2024-11-28 19:28:51 +0100

    Add StoreLocation fields.

--- src/storelocation.rs
+++ src/storelocation.rs
@@ -3,11 +3,16 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct StoreLocation {
-    pub store_location_id: u64,
+    pub store_location_id: Option<u64>,
     pub store_location_name: String,
     pub store_location_can_store: bool,
     pub store_location_color: Option<String>,
+    // Computed field on insert/update.
     pub store_location_full_path: Option<String>,
+
+    // Computed fields on select, not in DB.
+    pub store_location_nb_storage: Option<u64>,
+    pub store_location_nb_children: Option<u64>,
 
     pub entity: Option<Entity>,
     pub store_location: Option<Box<StoreLocation>>,
