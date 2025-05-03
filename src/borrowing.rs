812bd98b9795d9b04789d6f529961f1aa424983c
Thomas Bellembois <thomas.bellembois@uca.fr>
2025-05-03 20:50:18 +0200

    Add storage borrow.

--- src/borrowing.rs
+++ src/borrowing.rs
@@ -5,6 +5,6 @@ pub struct Borrowing {
     pub borrowing_id: u64,
     pub borrowing_comment: Option<String>,
     pub person: u64,
-    pub borrower: u64,
     pub storage: u64,
+    pub borrower: u64,
 }
